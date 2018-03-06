06f471b71d44b4e83db30953cc9236456ad0d812
Vadim Savenkov <vadim.savenkov@wu.ac.at>
2018-03-06 02:49:10 +0100

    logos etc

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
   });
 
   $("nav ul li,section").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 220;
+    var position = $($(this).attr("href")).offset().top - 190;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
